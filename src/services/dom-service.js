54c9cefab84fcc43d0ffa240d436aece1eae6208
Paul Vallis <vallis1@gmail.com>
2017-09-02 16:24:25 +0100

    Moves toast to index. Uses classes

--- src/services/dom-service.js
+++ src/services/dom-service.js
@@ -1,41 +1,14 @@
-const appendChild = (elem, id, style, parent) => {
-  const element = document.createElement(elem);
-  element.id = id;
-
-  element.setAttribute('style', style);
-
-  const parentElem = parent || document.body;
-  parentElem.appendChild(element);
-  return element;
-};
-
-const timedRemoveElement = (removeAfter, elem) => {
-  setTimeout(() => {
-    if (!elem) {
-      return;
-    }
-    elem.parentNode.removeChild(elem);
-  }, removeAfter);
-};
-
-export const showToast = (message, displayFor = 5000) => {
-  const toastStyle = `
-    width: 50vw;
-    background: #444;
-    color: #fff;
-    position: absolute;
-    top: 30%;
-    left: 25%;
-    z-index: 9999;
-    border-radius: 30px;
-    text-align: center;
-    padding: 0 8px;
-  `;
-
-  const toast = document.getElementById('toast') || appendChild('div', 'toast', toastStyle);
-  const toastMessage =
-    document.getElementById('toast-message') || appendChild('h3', 'toast-message', null, toast);
+const showToast = (message, displayFor = 5000) => {
+  const toast = document.getElementById('toast');
+  const toastMessage = document.getElementById('toast-message');
   toastMessage.innerText = message;
 
-  timedRemoveElement(displayFor, toast);
+  toast.classList.remove('hidden');
+  toast.classList.add('showing');
+  setTimeout(() => {
+    toast.classList.remove('showing');
+    toast.classList.add('hidden');
+  }, displayFor);
 };
+
+export default showToast;
