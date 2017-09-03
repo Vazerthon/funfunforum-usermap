b3f7e53bbf174d5a6f442b24d8315dd15d5d11cd
Vallis <paul@mrvallis.co.uk>
2017-09-03 17:13:56 +0100

    Moves markers to groups

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles.css';
 import {
   initMap,
   addMapMarker,
+  addGroupMarker,
   fetchForumData,
   extractUserLocations,
   showToast,
@@ -32,17 +33,7 @@ const combineCaptions = multiUserLocation => ({
   `,
 });
 
-const addUserMarkers = async () => {
-  const forumDataResult = await fetchForumData();
-  if (!forumDataResult.ok) {
-    showToast(`Problem loading forum data: ${forumDataResult.statusText}`);
-    return;
-  }
-  const forumData = await forumDataResult.json();
-  if (!Array.isArray(forumData)) {
-    showToast('Problem loading forum data: expected an array');
-    return;
-  }
+const addUserMarkers = (forumData) => {
   const userLocationData = extractUserLocations(forumData);
   userLocationData.map(addCaptions).map(combineCaptions).map(l =>
     addMapMarker({
@@ -53,9 +44,40 @@ const addUserMarkers = async () => {
     }),
   );
 
+  return userLocationData.length;
+};
+
+const addGroupMarkers = (forumData) => {
+  const userLocationData = extractUserLocations(forumData, 2);
+  userLocationData.map(addCaptions).map(combineCaptions).map(l =>
+    addGroupMarker({
+      lat: l.coords.lat,
+      lng: l.coords.lng,
+      caption: l.caption,
+      count: l.users.length,
+    }),
+  );
+
+  return userLocationData.length;
+};
+
+const loadMapData = async () => {
+  const forumDataResult = await fetchForumData();
+  if (!forumDataResult.ok) {
+    showToast(`Problem loading forum data: ${forumDataResult.statusText}`);
+    return;
+  }
+  const forumData = await forumDataResult.json();
+  if (!Array.isArray(forumData)) {
+    showToast('Problem loading forum data: expected an array');
+  }
+
+  const usersWithLocations = addUserMarkers(forumData);
+  addGroupMarkers(forumData);
+
   showToast(
-    `Loaded forum data for ${forumData.length} users, ${userLocationData.length} of them have a location set`,
+    `Loaded forum data for ${forumData.length} users, ${usersWithLocations} of them have a location set`,
   );
 };
 
-addUserMarkers();
+loadMapData();
--- src/services/user-map-service.js
+++ src/services/user-map-service.js
@@ -1,10 +1,12 @@
-import { map, tileLayer, marker, icon, divIcon } from 'leaflet';
+import { map, tileLayer, marker, icon, divIcon, layerGroup, control } from 'leaflet';
 import '../../node_modules/leaflet/dist/leaflet.css';
 import markerIcon2x from '../../node_modules/leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from '../../node_modules/leaflet/dist/images/marker-icon.png';
 import { profileImage } from './';
 
 let userMap;
+let userMarkers;
+let groupMarkers;
 
 const defaultIcon = icon({
   iconRetinaUrl: markerIcon2x,
@@ -23,8 +25,18 @@ const userIcon = username =>
     html: `<img class="marker-user-icon__image" src="${profileImage(username)}" />`,
   });
 
+const groupMarker = count =>
+  divIcon({
+    popupAnchor: [0, 0],
+    iconSize: [100, 100],
+    iconAnchor: [10, 10],
+    className: 'marker-group-icon',
+    html: `<h3>${count}</h3>`,
+  });
+
 export const initMap = (hostId) => {
   userMap = map(hostId, { worldCopyJump: true });
+  userMap.setView([55.942, -3.21], 3);
 
   const tiles = tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -32,13 +44,33 @@ export const initMap = (hostId) => {
     maxZoom: 18,
   });
 
-  userMap.setView([55.942, -3.21], 3);
   userMap.addLayer(tiles);
+
+  userMarkers = layerGroup([]);
+  groupMarkers = layerGroup([]);
+
+  const baseMaps = {
+    'Open Street Map': tiles,
+  };
+
+  const overlayMaps = {
+    Users: userMarkers,
+    Groups: groupMarkers,
+  };
+
+  control.layers(baseMaps, overlayMaps).addTo(userMap);
 };
 
 export const addMapMarker = ({ username, lat, lng, caption }) => {
   const mark = marker([lat, lng], {
     icon: username ? userIcon(username) : defaultIcon,
-  }).addTo(userMap);
+  }).addTo(userMarkers);
+  mark.bindPopup(caption);
+};
+
+export const addGroupMarker = ({ lat, lng, caption, count }) => {
+  const mark = marker([lat, lng], {
+    icon: groupMarker(count),
+  }).addTo(groupMarkers);
   mark.bindPopup(caption);
 };
